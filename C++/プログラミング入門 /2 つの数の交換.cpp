4567c69a5ec33148446f9648b0baf7dedf55e698
Cristian Rian <127137723+otoyaa@users.noreply.github.com>
2023-10-05 14:33:28 -0300

    papurika

---
+++ C++/プログラミング入門 /2 つの数の交換.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+#define read(x) cin>>x
+#define vi vector<int>
+#define l(i,s,n) for (int i=int(s);i<int(n);i++)
+#define pb push_back
+#define f first
+#define s second
+#define no cout<<"NO"<<"\n"
+#define yes cout<<"YES"<<"\n"
+#define pii pair<int,int>
+typedef long long ll;
+using namespace std;
+
+void solve(){
+    vi v;vi v2;
+    while(1){
+        int a,b;cin>>a>>b;
+        if(a==0&&b==0){
+            break;
+        }
+
+         if(a<b){
+            int temp=a;
+            a=b,b=temp;
+         }
+         v.pb(a);v2.pb(b);
+    }
+    
+    l(i,0,v.size()){
+        cout<<v[i]<<" "<<v2[i]<<'\n';
+    }
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(nullptr);
+    cout.tie(0);   
+    
+    solve();
+}
