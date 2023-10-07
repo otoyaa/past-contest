59eaf4e3e21e7c1939d7bd06538f85dae06c7e5b
Cristian Rian <127137723+otoyaa@users.noreply.github.com>
2023-10-07 13:00:14 -0300

    kirameku kaze

---
+++ C++/プログラミング入門 /数列の反転.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define read(x) cin>>x
+#define vi vector<int>
+#define l(i,s,n) for (int i=int(s);i<int(n);i++)
+#define pb push_back
+#define f first
+#define s second
+#define no cout<<"NO"<<"\n"
+#define yes cout<<"YES"<<"\n"
+#define pii pair<int,int>
+typedef long long ll;
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(nullptr);
+    cout.tie(0);   
+    
+    int n;read(n);
+    vi v(n);
+    for(int i=0;i<n;i++)read(v[i]);
+    reverse(v.begin(),v.end());
+    for(int i=0;i<n;i++){
+        cout<<v[i]<<(i==n-1?"\n":" ");
+    }
+}
