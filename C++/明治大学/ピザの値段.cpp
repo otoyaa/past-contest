83efebee0dbea3157ba8fa27ba24c4b2969d66be
Cristian Rian <127137723+otoyaa@users.noreply.github.com>
2023-10-09 09:38:01 -0300

    Create ピザの値段.cpp

---
+++ C++/明治大学/ピザの値段.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define read(x) cin>>x
+#define vi vector<int>
+#define l(i,s,n) for (int i=int(s);i<int(n);i++)
+#define pb push_back
+#define f first
+#define s second
+#define no cout<<"NO"<<"\n"
+#define yes cout<<"YES"<<"\n"
+#define pii pair<int,int>
+typedef long long ll;
+using namespace std;
+
+void solve(){
+    int a,b;cin>>a>>b;
+    if(a>=1000&&b<=1000||b>=1000&&a<=1000){
+        cout<<max(a,b)<<"\n";
+    }else{
+        cout<<a+b<<"\n";
+    }
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(nullptr);
+    cout.tie(0);   
+    
+    solve();
+}
