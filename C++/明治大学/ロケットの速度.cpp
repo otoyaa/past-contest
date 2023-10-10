b658a4dc81583a246d0ca3780d17c6c4c9487e9d
Cristian Rian <127137723+otoyaa@users.noreply.github.com>
2023-10-10 10:00:38 -0300

    Create ロケットの速度.cpp

---
+++ C++/明治大学/ロケットの速度.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define read(x) cin>>x
+#define vi vector<int>
+#define l(i,s,n) for (int i=int(s);i<int(n);i++)
+#define pb push_back
+#define f first
+#define s second
+#define no cout<<"NO"<<"\n"
+#define yes cout<<"YES"<<"\n"
+#define pii pair<int,int>
+typedef long long ll;
+using namespace std;
+
+void solve(){
+    double m,f,t;cin>>m>>f>>t;
+    // F=m*t
+    cout<<fixed<<setprecision(2)<<m*t<<"\n";
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(nullptr);
+    cout.tie(0);   
+    
+    solve();
+}
