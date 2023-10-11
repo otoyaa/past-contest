6b9aa7ccf24ed7e1b5772459c6748e803551a0e2
Cristian Rian <127137723+otoyaa@users.noreply.github.com>
2023-10-11 17:40:53 -0300

    isupper(), islower() !upper and !lower

---
+++ C++/Japan High School Programming Contest - Preliminares/2019/アスキー文字.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#define read(x) cin>>x
+#define vi vector<int>
+#define l(i,s,n) for (int i=int(s);i<int(n);i++)
+#define pb push_back
+#define f first
+#define s second
+#define no cout<<"NO"<<"\n"
+#define yes cout<<"YES"<<"\n"
+#define pii pair<int,int>
+typedef long long ll;
+using namespace std;
+
+void solve(){
+    int n;read(n);
+    char ans=char(n);
+    if(isupper(ans)){
+        cout<<1<<"\n";
+    }
+    if(islower(ans)){
+        cout<<2<<"\n";
+    }
+    if(!isupper(ans)&&!islower(ans)){
+        cout<<0<<"\n";
+    }
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(nullptr);
+    cout.tie(0);   
+    
+    solve();
+}
