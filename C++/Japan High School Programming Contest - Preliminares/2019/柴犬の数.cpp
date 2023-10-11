85fc449a979243f9b747f5b89adca78a40bd5ae2
Cristian Rian <127137723+otoyaa@users.noreply.github.com>
2023-10-11 17:06:36 -0300

    only sum

---
+++ C++/Japan High School Programming Contest - Preliminares/2019/柴犬の数.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define read(x) cin>>x
+#define vi vector<int>
+#define l(i,s,n) for (int i=int(s);i<int(n);i++)
+#define pb push_back
+#define f first
+#define s second
+#define no cout<<"NO"<<"\n"
+#define yes cout<<"YES"<<"\n"
+#define pii pair<int,int>
+typedef long long ll;
+using namespace std;
+
+void solve(){
+    int r,b,w,g;cin>>r>>b>>w>>g;
+    cout<<r+b+w+g<<"\n";
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(nullptr);
+    cout.tie(0);   
+    
+    solve();
+}
