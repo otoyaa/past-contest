0373039b7be5fcd5596884fd60719c2c8a895e59
Cristian Rian <127137723+otoyaa@users.noreply.github.com>
2023-10-10 21:15:44 -0300

    Create キャンディの分配.cpp

---
+++ C++/Japan High School Programming Contest - Preliminares/キャンディの分配.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define read(x) cin>>x
+#define vi vector<int>
+#define l(i,s,n) for (int i=int(s);i<int(n);i++)
+#define pb push_back
+#define f first
+#define s second
+#define no cout<<"NO"<<"\n"
+#define yes cout<<"YES"<<"\n"
+#define pii pair<int,int>
+typedef long long ll;
+using namespace std;
+
+void solve(){
+    int a,n;cin>>a>>n;
+    n=n+1;
+    cout<<a%n<<"\n";
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(nullptr);
+    cout.tie(0);   
+    
+    solve();
+}
